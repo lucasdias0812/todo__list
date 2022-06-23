f70ae9c8aa9ab15d055120e0b4087eba14470837
lucasdias0812 <99883219+lucasdias0812@users.noreply.github.com>
2022-06-23 09:10:51 -0300

    Update servidor.js

--- config/servidor.js
+++ config/servidor.js
@@ -3,7 +3,7 @@ const express = require('express')
 //executar o express
 const app = express()
 //definir a porta do servidor local
-const porta = 3030
+const porta = process.env.PORT || 3030
 
 //definir a pasta dos arquivos estaticos (css, imagens, jquery)
 app.use(express.static('./assets'))
